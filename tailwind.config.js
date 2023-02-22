807d7bef895a8b2a593645297f1b398c35957d34
hRJ06 <123245585+hRJ06@users.noreply.github.com>
2023-02-22 11:55:34 +0530

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+    fontFamily:{
+      Whitney:["Whitney"],
+      Ginto:["Ginto"],
+      GintoNord:["GintoNord"],
+      ggSans:["ggSans"],
+    },
+    colors: {
+    },},
+  },
+  plugins: [],
+}
